1670187b846bceddf15435fad469ba00869df623
amatepl <mateusz.szydelko.ms@gmail.com>
2019-03-19 23:48:01 +0100

    Comment

---
+++ Main.m
@@ -0,0 +1,13 @@
+%-------Symbol mapping and demapping-----
+
+%-------Nyquist Filter------------
+
+% 1 MHz cutoff fre-quency
+% 0.3 roll-off factor  
+% Before filtering, the symbol sequence is over-sampled with a factor M >
+% 1.
+% The sample rate fixes the bandwidth simulated in Matlab, that must be high
+% enough to simulate the halfroot Nyquist filtering.
+
+
+%-------AWGN----------------------
