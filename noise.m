d4e61db84b642fc6e39eafd6a1191b8c12c12a22
cyprienmaes <cyprienmaes@gmail.com>
2019-03-29 18:21:06 +0100

    Add of noise in the signal_tx

---
+++ noise.m
@@ -0,0 +1,9 @@
+function signal_noise = noise(signal_tx,Eb_No,Fsampling,Nb)
+signal_energy = (trapz(abs(signal_tx).^2))*(1/Fsampling);
+Eb = signal_energy/Nb;
+Eb = Eb/2;
+No = Eb/Eb_No;
+noise_power = 2*No*Fsampling;
+noise = ones(length(signal_tx),1);
+noise = sqrt(noise_power/2)*(randn(length(signal_tx),1)+1i*randn(length(signal_tx),1));
+signal_noise = signal_tx + noise;
--- test.m
+++ test.m
@@ -3,11 +3,15 @@ Fsymbol = 1e6;
 beta = 0.3;
 RRCTaps = 133;
 Fsampling = 8e6;
+Nb = 10000;
 
 % Creation of a random sequence of bits for the moment (row vector)
-bit_tx = randi([0 1],10000, 1);
+bit_tx = randi([0 1],Nb, 1);
 Nbps = 2; % Bit per symbol 
 % Upsampling factor 
 M = 4;
+
+Eb_No = 2;
 filter = RRCFilter(beta,Fsymbol,Fsampling, RRCTaps);
 signal_tx = TX(bit_tx,filter,Nbps,M);
+signal_noise = noise(signal_tx,Eb_No,Fsampling,Nb);
