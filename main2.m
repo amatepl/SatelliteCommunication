1a9e5c7f71d8f9ae7bcd8a8a46df0820d38c9c53
cyprienmaes <cyprienmaes@gmail.com>
2019-05-06 17:54:22 +0200

    BER works

--- main2.m
+++ main2.m
@@ -26,55 +26,53 @@ coderate = 1/2;  % ex : 1/2 => 1 check bit for 1 bit
 codesize = blocksize/coderate;
 
 Nbps = 1;
-Eb_No_dB = 12:1:12;
+Eb_No_dB = 2:1:5;
 Nb = Nbps*1e3*blocksize;
-
-% Creation of a random sequence of bits for the moment (row vector)
-bit_tx = randi([0 1],Nb,1);
 
 %% LDPC Encoder and generation of the parity check matrix
 H = makeLdpc(blocksize,codesize,0,1,3);
 % The bits transmitted are made in blocks
-bit_tx = reshape(bit_tx,blocksize,Nb/blocksize);
+% Creation of a random sequence of bits for the moment (row vector)
+bit_tx = randi([0 1],blocksize,Nb/blocksize);
 % Generate parity check bits in function of each blocks
 [parity_bit,Hnew] = makeParityChk(bit_tx,H,1);
-LDPC_bits_tx = vertcat(bit_tx,parity_bit);
+LDPC_bits_tx = [parity_bit;bit_tx];
 
 %% Filter + noise :
 % Filter that is used :
 filter = RRCFilter(beta,Fsymbol,Fsampling, RRCTaps);
 
-LDPC_bits_rx = zeros(codesize,Nb/blocksize);
+BER = zeros(1,length(Eb_No_dB));
+bits_tx = LDPC_bits_tx(:);
 
 for j = 1:length(Eb_No_dB)
+    LDPC_bits_rx = zeros(codesize,Nb/blocksize);
     for k = 1:length(LDPC_bits_rx)
         % TX side :
         [symb_tx,signal_tx] = TX(LDPC_bits_tx(:,k), filter,Nbps, M);
 
-        BER_coded = zeros(1,length(Eb_No_dB));
-        BER_uncoded = zeros(1,length(Eb_No_dB));
         % Add noise on the signal :
-        signal_rx = noise(signal_tx, Eb_No_dB(j),Fsampling,Nb);
+        signal_rx = noise(signal_tx, Eb_No_dB(j),Fsampling,codesize);
 
         % RX side :
         [symb_rx,bits_rx] = RX(signal_rx, filter,Nbps, M, RRCTaps);
         LDPC_bits_rx(:,k) = tanner(bits_rx,Hnew,2);
     end
-%     errors_coded = abs(LDPC_bits_tx-LDPC_bits_rx);
-%     BER_coded(j) = 1e6*(sum(errors_coded)*coderate)*1/Nb*1e-6;
+    bits_rx = LDPC_bits_rx(:);
+    error= abs(bits_tx-bits_rx);
+    BER(j) = 1e6*(sum(error)*coderate)*1/Nb*1e-6;
 end
-
-% figure();
-% berTheo = berawgn(Eb_No_dB,'pam',2);
-% semilogy(Eb_No_dB,BER_coded,'-o');
+figure();
+berTheo = berawgn(Eb_No_dB,'pam',2);
+semilogy(Eb_No_dB,BER_coded,'-o');
+hold on;
+% semilogy(Eb_No_dB,BER_uncoded,'-o');
 % hold on;
-% % semilogy(Eb_No_dB,BER_uncoded,'-o');
-% % hold on;
-% semilogy(Eb_No_dB,berTheo,'-o');
-% title('BER of an ideal channel in satellite communication with AWGN PAM');
-% legend('LDPC hard decoding coded 20 it','Theorical','Location', 'Best');
-% xlabel('Eb/N0 (dB)');
-% ylabel('BER');
-% grid on;
+semilogy(Eb_No_dB,berTheo,'-o');
+title('BER of an ideal channel in satellite communication with AWGN PAM');
+legend('LDPC hard decoding coded 20 it','Theorical','Location', 'Best');
+xlabel('Eb/N0 (dB)');
+ylabel('BER');
+grid on;
 
 
